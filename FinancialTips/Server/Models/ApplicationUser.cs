5e4f7947b15dd989016fc57db42dcf34e07d8b77
UMESH UDAYAKUMAR 2100053D <2100053D@Student.tp.edu.sg>
2022-12-18 17:27:03 +0800

    added images

--- FinancialTips/Server/Models/ApplicationUser.cs
+++ FinancialTips/Server/Models/ApplicationUser.cs
@@ -8,5 +8,7 @@ namespace FinancialTips.Server.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
